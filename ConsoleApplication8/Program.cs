0e1e68cecb8ca8217f216ce576a207cd5b1dcfe8
Abdalrazag <abode9736@gmail.com>
2017-03-25 00:58:37 +0200

    float
    max & min float

---
+++ ConsoleApplication8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is my first Programe
+
+           
+            float min = float.MinValue;
+            float max = float.MaxValue;
+
+            Console.WriteLine("min ="+min + " max ="+max);
+            Console.ReadKey();
+
+        }
+    }
+    
+}
