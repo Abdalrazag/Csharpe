b1e8650f632669aae6159d5aae9805bb66c1e040
Abdalrazag <abode9736@gmail.com>
2017-03-27 00:35:17 +0200

    Q10
    Switch

---
+++ Q10_Switch/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q10_Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string What_He_Say = "Hello";
+
+            switch (What_He_Say)
+            {
+
+               case "hello":
+
+                   Console.WriteLine("Hellow"); break;
+
+               case "Bye":
+
+                    Console.WriteLine("Bye"); break;
+
+               case "Welcome":
+
+                    Console.WriteLine("Welcome"); break;
+
+               default:
+
+                    Console.WriteLine("No One"); break;
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
